7b35d8d92b26c8b35f3228456e8932cdb5d7ab31
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2018-02-17 23:53:13 +0600

    searching and sorting in student list implemented

---
+++ app/src/main/java/shadattonmoy/ams/SortingOptionBottomSheet.java
@@ -0,0 +1,84 @@
+package shadattonmoy.ams;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import shadattonmoy.ams.attendance.ClassInstance;
+
+/**
+ * Created by Shadat Tonmoy on 2/17/2018.
+ */
+
+public class SortingOptionBottomSheet extends BottomSheetDialogFragment {
+
+    private LinearLayout bottomSheetRegNoMenu,bottomSheetNameMenu,bottomSheetTypeMenu;
+    private Course course;
+    private ClassInstance classInstance;
+    private TakeAttendanceStudentList takeAttendanceStudentList;
+    @Override
+    public void setupDialog(final Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.sorting_option_bottom_sheet, null);
+        initNodes(contentView);
+        setNodesTouchListener();
+        dialog.setContentView(contentView);
+    }
+
+    /*
+    * method to initialize bottom sheet nodes
+    * */
+
+    public void initNodes(View view)
+    {
+        bottomSheetRegNoMenu = (LinearLayout) view.findViewById(R.id.sort_bottom_sheet_reg_no);
+        bottomSheetNameMenu = (LinearLayout) view.findViewById(R.id.sort_bottom_sheet_name);
+
+        bottomSheetTypeMenu = (LinearLayout) view.findViewById(R.id.sort_bottom_sheet_type);
+    }
+
+
+
+
+    /*
+            * method to handle bottom sheet menu click
+            * */
+    public void setNodesTouchListener()
+    {
+        bottomSheetRegNoMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getActivity().getApplicationContext(),"Sort By RegNo ",Toast.LENGTH_SHORT).show();
+                takeAttendanceStudentList.setSortingParameter("RegNo");
+                //takeAttendanceStudentList.sortStudentList("RegNo");
+
+            }
+        });
+
+        bottomSheetNameMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(getActivity().getApplicationContext(),"Sort By Name ",Toast.LENGTH_SHORT).show();
+                takeAttendanceStudentList.setSortingParameter("Name");
+                //takeAttendanceStudentList.sortStudentList("Name");
+            }
+        });
+
+        bottomSheetTypeMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(getActivity().getApplicationContext(),"Sort By Type ",Toast.LENGTH_SHORT).show();
+                takeAttendanceStudentList.setSortingParameter("Type");
+            }
+        });
+    }
+
+    public void setTakeAttendanceStudentList(TakeAttendanceStudentList takeAttendanceStudentList) {
+        this.takeAttendanceStudentList = takeAttendanceStudentList;
+    }
+}
--- app/src/main/java/shadattonmoy/ams/TakeAttendanceStudentList.java
+++ app/src/main/java/shadattonmoy/ams/TakeAttendanceStudentList.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
@@ -27,6 +30,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import shadattonmoy.ams.attendance.ClassInstance;
@@ -51,6 +55,9 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
     private StudentAdapter studentAdapter;
     private ClassInstanceStudentList classInstanceStudentList;
     private int classInstanceWeight;
+    private SortingOptionBottomSheet sortingOptionBottomSheet;
+    private String sortingParameter;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +70,9 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
         classInstanceStudentList = (ClassInstanceStudentList) getIntent().getSerializableExtra("StudentList");
         classInstanceDate = getIntent().getStringExtra("Date");
         students = classInstanceStudentList.getStudents();
-        Log.e("Class ID",classInstanceId+"");
         initialize();
+        sortStudentList("RegNo");
+        Log.e("Class ID",classInstanceId+"");
         initStudents();
     }
 
@@ -133,7 +141,6 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
             noStudentFoundMsg.setVisibility(View.GONE);
             studentAdapter = new StudentAdapter(TakeAttendanceStudentList.this,R.layout.student_single_row,R.id.student_icon,students);
             studentAdapter.setPresentFlagMap(presentMap);
-
             studentAdapter.setFragmentManager(getSupportFragmentManager());
             studentAdapter.setShowVertIcon(false);
             studentAdapter.setShowPresentAbsentRadio(true);
@@ -230,7 +237,6 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //Log.e("Total Attendacne","Size "+attendanceCursor.getCount());
         if(attendanceCursor.getCount()==0)
             insertAttendance();
         else updateAttendance();
@@ -242,12 +248,57 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.take_attendance_student_list_menu, menu);
+
+
+        MenuItem searchItem = menu.findItem(R.id.search_menu);
+        searchView = new SearchView(TakeAttendanceStudentList.this);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("Search Here");
+        searchView.setQueryHint(Html.fromHtml("<font color = #95a5a6>" + getResources().getString(R.string.search_hint) + "</font>"));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterData(newText);
+                return false;
+            }
+        });
         return true;
+    }
+
+    public void filterData(String query)
+    {
+        List<Student> filteredStudents= new ArrayList<Student>();
+        if(searchView!=null)
+        {
+            for(Student student:students)
+            {
+                if(student.getName().toLowerCase().startsWith(query.toLowerCase()) || student.getName().toLowerCase().endsWith(query.toLowerCase())|| student.getRegNo().toLowerCase().startsWith(query.toLowerCase()) || student.getRegNo().endsWith(query.toLowerCase()))
+                {
+                    filteredStudents.add(student);
+                }
+                //else filteredVendors=vendors;
+                studentAdapter = new StudentAdapter(TakeAttendanceStudentList.this,R.layout.student_single_row,R.id.student_reg_no,filteredStudents);
+                studentAdapter.setPresentFlagMap(presentMap);
+                studentAdapter.setFragmentManager(getSupportFragmentManager());
+                studentAdapter.setShowVertIcon(false);
+                studentAdapter.setShowPresentAbsentRadio(true);
+
+                studentList.setAdapter(studentAdapter);
+            }
+
+
+        }
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
         switch (item.getItemId()) {
             case R.id.search_menu:
                 Toast.makeText(TakeAttendanceStudentList.this,"Search",Toast.LENGTH_SHORT).show();
@@ -255,28 +306,88 @@ public class TakeAttendanceStudentList extends AppCompatActivity {
             case R.id.sort_menu:
             {
                 Toast.makeText(TakeAttendanceStudentList.this,"Sort",Toast.LENGTH_SHORT).show();
-                Collections.sort(students, new Comparator<Student>() {
-                    @Override
-                    public int compare(Student o1, Student o2) {
-                        if(o1.getRegNo().compareTo(o2.getRegNo())>0)
-                            return 1;
-                        else if(o1.getRegNo().compareTo(o2.getRegNo())<0)
-                            return -1;
-                        else return 0;
-                    }
-                });
-                studentAdapter = new StudentAdapter(TakeAttendanceStudentList.this,R.layout.student_single_row,R.id.student_reg_no,students);
-                studentAdapter.setPresentFlagMap(presentMap);
-                studentAdapter.setFragmentManager(getSupportFragmentManager());
-                studentAdapter.setShowVertIcon(false);
-                studentAdapter.setShowPresentAbsentRadio(true);
-                studentList.setAdapter(studentAdapter);
-                studentAdapter.notifyDataSetChanged();
+                sortingOptionBottomSheet = new SortingOptionBottomSheet();
+                sortingOptionBottomSheet.setTakeAttendanceStudentList(this);
+                sortingOptionBottomSheet.show(getSupportFragmentManager(),"Sorting Options");
             }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public String getSortingParameter() {
+        return this.sortingParameter;
+    }
+
+    public void setSortingParameter(String sortingParameter) {
+        this.sortingParameter = sortingParameter;
+        sortStudentList(this.sortingParameter);
+
+    }
+
+    public void sortStudentList(String parameter)
+    {
+        Toast.makeText(TakeAttendanceStudentList.this,"Sorting By "+parameter,Toast.LENGTH_SHORT).show();
+        try{
+            sortingOptionBottomSheet.dismiss();
+        }catch (Exception e)
+        {
+
+        }
+        if(parameter.equals("RegNo"))
+        {
+            Log.e("Sorting By","RegNo");
+            Collections.sort(students, new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    String regNo1 = o1.getRegNo();
+                    String regNo2 = o2.getRegNo();
+                    int roll1 = Integer.parseInt(regNo1.substring(regNo1.length()-3,regNo1.length()));
+                    int roll2 = Integer.parseInt(regNo2.substring(regNo2.length()-3,regNo2.length()));
+                    //Log.e("Roll","Roll 1 : "+Integer.parseInt(roll1)+" Roll 2 : "+Integer.parseInt(roll2));
+                    String session1 = regNo1.substring(0,5);
+                    String session2 = regNo2.substring(0,5);
+                    if(session1.compareTo(session2)==0)
+                    {
+                        if(roll1>roll2)
+                            return 1;
+                        else return -1;
+                    }
+                    if(session1.compareTo(session2)>=0)
+                        return -1;
+                    else return 1;
+                }
+            });
+            for (Student student:students)
+            {
+                Log.e("Reg No",student.getRegNo());
+            }
+
+        }
+        else if(parameter.equals("Name"))
+        {
+            Collections.sort(students, new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    String name1 = o1.getName();
+                    String name2 = o2.getName();
+                    if(name1.compareTo(name2)>=0)
+                        return 1;
+                    else if(name1.compareTo(name2)<=0)
+                        return -1;
+                    else return 0;
+                }
+            });
+        }
+        //studentAdapter.clear();
+        studentAdapter = new StudentAdapter(TakeAttendanceStudentList.this,R.layout.student_single_row,R.id.student_reg_no,students);
+        studentAdapter.setPresentFlagMap(presentMap);
+        studentAdapter.setFragmentManager(getSupportFragmentManager());
+        studentAdapter.setShowVertIcon(false);
+        studentAdapter.setShowPresentAbsentRadio(true);
+        studentList.setAdapter(studentAdapter);
+        studentAdapter.notifyDataSetChanged();
     }
 }
 
