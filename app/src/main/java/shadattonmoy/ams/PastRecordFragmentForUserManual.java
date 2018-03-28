45a882a715df724d4c348493ef763e5f9028b818
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2018-03-29 01:02:07 +0600

    past record user manual added.....and everything is almost done....waiting for user feedback for further improvement

--- app/src/main/java/shadattonmoy/ams/PastRecordFragmentForUserManual.java
+++ app/src/main/java/shadattonmoy/ams/PastRecordFragmentForUserManual.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class PastRecordFragmentForUserManual extends Fragment {
     private View view;
+    private WebView webView;
 
     public PastRecordFragmentForUserManual() {
         // Required empty public constructor
@@ -26,6 +30,12 @@ public class PastRecordFragmentForUserManual extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_past_record_fragment_for_user_manual, container, false);
+        webView = (WebView) view.findViewById(R.id.past_record_web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setVerticalScrollBarEnabled(false);
+        webView.loadUrl("file:///android_asset/past_records.html");
         return view;
     }
 }
